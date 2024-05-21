109e229e5b58dcb1b2252abeba92078fbc0a05e4
mailo4 <mail.nuriev@narxoz.kz>
2024-05-21 14:09:41 +0600

    first

---
+++ src/info/Info.js
@@ -0,0 +1,144 @@
+import self from "../img/self.png"
+import mock1 from "../img/mock1.png"
+import mock2 from "../img/mock2.png"
+import mock3 from "../img/mock3.png"
+import mock4 from "../img/mock4.png"
+import mock5 from "../img/mock5.png"
+
+/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
+how to make this template work best for you! */
+
+export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+/*
+I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
+These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
+the site.
+ */
+
+/* 
+This variable will change the layout of the website from multipage to single, scrollable page
+*/
+export let singlePage = false;
+
+/*
+So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
+that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
+of what each of the values mean.
+ */
+
+export const info = {
+    firstName: "Mail",
+    lastName: "Nuriev",
+    initials: "mn", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a Full Stack Developer",
+    selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
+    gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+    baseColor: colors[0],
+    miniBio: [ // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
+        {
+            emoji: '☕',
+            text: 'I work out at the gym'
+        },
+        {
+            emoji: '🌎',
+            text: 'I live in Kazakhstan'
+        },
+        {
+            emoji: "💼",
+            text: "Student of Narkhoz University"
+        },
+        {
+            emoji: "📧",
+            text: "mail.nuriev@narxoz.kz"
+        }
+    ],
+    socials: [
+        {
+            link: "https://ru-ru.facebook.com/",
+            icon: 'fa fa-facebook',
+            label: 'facebook'
+        },
+        {
+            link: "https://www.instagram.com/mail_o4",
+            icon: 'fa fa-instagram',
+            label: 'instagram'
+        },
+        {
+            link: "https://github.com",
+            icon: "fa fa-github",
+            label: 'github'
+        },
+        {
+            link: "https://linkedin.com",
+            icon: "fa fa-linkedin",
+            label: 'linkedin'
+        },
+        {
+            link: "https://x.com/?lang=ru",
+            icon: "fa fa-twitter",
+            label: 'twitter'
+        }
+// Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
+// Just change the links so that they lead to your social profiles.
+
+    ],
+    bio: "Hi, I'm Mail. A student of Narkhoz University, I study computer science, I love the long road and I believe that artificial intelligence will inevitably rule us one day. You have to hire me!",
+    skills:
+        {
+            proficientWith: ['javascript', 'react', 'git', 'github', 'html5', 'css3', 'figma'],
+            exposedTo: ['nodejs', 'python']
+        }
+    ,
+    hobbies: [
+        {
+            label: 'reading',
+            emoji: '📖'
+        },
+        {
+            label: 'theater',
+            emoji: '🎭'
+        },
+        {
+            label: 'movies',
+            emoji: '🎥'
+        },
+        {
+            label: 'sport',
+            emoji: '🌶'
+        }
+// Same as above, change the emojis to match / relate to your hobbies or interests.
+// You can also remove the emojis if you'd like, I just think they look cute :P
+    ],
+    portfolio: [ // This is where your portfolio projects will be detailed
+        {
+            title: "Project 1",
+            live: "https://github.com/mailo4/Mail", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            source: "https://github.com/mailo4/Mail", // this should be a link to the **repository** of the project, where the code is hosted.
+            image: mock1
+        },
+        {
+            title: "Project 2",
+            live: "https://github.com/mailo4/to-do-mail",
+            source: "https://github.com/mailo4/to-do-mail",
+            image: mock2
+        },
+        {
+            title: "Project 3",
+            live: "https://github.com/mailo4/mailo4",
+            source: "https://github.com/mailo4/mailo4",
+            image: mock3
+        },
+        {
+            title: "Project 4",
+            live: "https://github.com/j1ter/PROProject",
+            source: "https://github.com/j1ter/PROProject",
+            image: mock4
+        },
+        {
+            title: "Project 5",
+            live: "https://github.com/mailo4/kkinoo",
+            source: "https://github.com/mailo4/kkinoo",
+            image: mock5
+        }
+    ]
+}
